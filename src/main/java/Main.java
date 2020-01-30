b05650773a0bfb43d4c5a3295fae5260575e8d69
Roman Mikhel <r.s.mikhel@ya.ru>
2020-01-30 13:18:07 +0300

    Init

---
+++ src/main/java/Main.java
@@ -0,0 +1,33 @@
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
+import java.sql.*;
+
+public class Main {
+    public static void main(String[] args) throws SQLException {
+
+
+        StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
+                .configure("hibernate.cfg.xml")
+                .build();
+        Metadata metadata = new MetadataSources(registry).getMetadataBuilder().build();
+        SessionFactory sessionFactory = metadata.getSessionFactoryBuilder().build();
+
+        Session session = sessionFactory.openSession();
+
+        Course course = session.get(Course.class, 3);
+
+        System.out.println("Курс " + course.getName() + " длится всего " + course.getDuration() + " часов. Имеет описание:\n" + course.getDescription());
+
+        System.out.println(course.getName());
+
+
+        session.close();
+
+
+    }
+}
